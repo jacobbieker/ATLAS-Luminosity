432f7f4c3c429d3a452e716f33c49c382ec8db44
Jacob Bieker <jacob@bieker.tech>
2016-01-20 11:23:10 -0800

    Glob kinda works

--- luminosity-time.py
+++ luminosity-time.py
@@ -1,1 +1,8 @@
 __author__ = 'Jacob Bieker'
+#import rootpy as ROOT
+import os
+import re
+import glob
+# Go through each directory of luminosity data
+
+print(glob.glob(os.path.join("data", "4557", "4557_lumi_???_ATLAS.txt")))
