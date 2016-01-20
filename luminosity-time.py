a35d99e4ec89238891a3c83a6f7fc18594ce0a0d
Jacob Bieker <jacob@bieker.tech>
2016-01-20 13:52:01 -0800

    Opening each data file

--- luminosity-time.py
+++ luminosity-time.py
@@ -1,8 +1,13 @@
 __author__ = 'Jacob Bieker'
 #import rootpy as ROOT
 import os
-import re
 import glob
+
 # Go through each directory of luminosity data
 
+# ATLAS 4557 Data
 atlas_4557_data = glob.iglob(os.path.join("data", "4557", "4557_lumi_*_ATLAS.txt"))
+
+for atlas_file in atlas_4557_data:
+    with open(atlas_file, 'r') as data:
+        print("Opened " + str(atlas_file) + "\n")
