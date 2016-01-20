0c82f83d9580c56ae9e97462dfb47ebad9d28e72
Jacob Bieker <jacob@bieker.tech>
2016-01-20 11:53:13 -0800

    Switch to iterator

--- luminosity-time.py
+++ luminosity-time.py
@@ -5,4 +5,4 @@ import re
 import glob
 # Go through each directory of luminosity data
 
-print(glob.glob(os.path.join("data", "4557", "4557_lumi_???_ATLAS.txt")))
+atlas_4557_data = glob.iglob(os.path.join("data", "4557", "4557_lumi_*_ATLAS.txt"))
