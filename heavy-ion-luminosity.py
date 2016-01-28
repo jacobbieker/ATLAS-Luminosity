49539431d0e4172c5ce83c20f436b58e41d0fa02
Jacob Bieker <jacob@bieker.tech>
2016-01-27 19:20:59 -0800

    Add getting data from .root file into Numpy

---
+++ heavy-ion-luminosity.py
@@ -0,0 +1,22 @@
+__author__ = 'jacob'
+import ROOT
+import numpy as np
+import os
+from root_numpy import root2array, root2rec, tree2rec
+
+# Look at r284484 data
+
+filename = os.path.join("data", "r284484.root")
+
+# Convert a TTree in a ROOT file into a NumPy structured array
+arr = root2array(filename)
+
+for element in arr:
+    print(element)
+# The TTree name is always optional if there is only one TTree in the file
+
+# Convert a TTree in a ROOT file into a NumPy record array
+rec = root2rec(filename)
+
+# Get the TTree from the ROOT file
+rfile = ROOT.TFile(filename)
