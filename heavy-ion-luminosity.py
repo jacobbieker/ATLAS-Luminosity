35a2749000704deb731fe7c28930555e5e0b6c9b
Jacob Bieker <jacob@bieker.tech>
2016-02-09 11:11:17 -0800

    Moved plotting to another file
    For future plotting and easier to read main files

--- heavy-ion-luminosity.py
+++ heavy-ion-luminosity.py
@@ -3,12 +3,10 @@ from __future__ import division
 __author__ = 'jacob'
 import ROOT
 import numpy as np
-from rootpy.plotting import Canvas, Graph
-from rootpy.plotting.style import get_style, set_style
-from rootpy.interactive import wait
 import os
 import math
-from root_numpy import root2array, root2rec, tree2rec
+from root_numpy import root2array
+import luminosity_plotting_routines as luminosity_plotting
 
 # Look at r284484 data
 
@@ -98,50 +96,9 @@ for block in range(len(luminosity_block)):
     luminosity_ratio_lucid_v_sum[block].append(luminosity_ratio_lucid_v)
     luminosity_ratio_h_v_sum[block].append(luminosity_ratio_h_v)
 
+# Actually plot the luminosity ratios
+luminosity_plotting.plot_luminosity_ratio(lucid_event_or_bi1, bcm_v_event_or1, luminosity_block, 'ATLAS')
 
-def plot_luminosity_ratio(detector_one_data, detector_two_data, luminosity_blocks, style):
-    '''
+luminosity_plotting.plot_luminosity_ratio(lucid_event_or_bi1, bcm_h_event_or1, luminosity_block, 'ATLAS')
 
-    :param detector_one_data: Data from one detector type, such as ATLAS' LUCID, in a list of lists, every entry is one
-    luminsoity block, with the luminosity block being a list of BCID data, assumed to be same
-    length as detector_two_data
-    :param detector_two_data: Data from another detector type, such as ATLAS' LUCID, in a list of lists, every entry is
-    one luminsoity block, with the luminosity block being a list of BCID data, assumed to be same
-    length as detector_one_data
-    :param luminosity_blocks: A list of luminosity blocks
-    :param style: The ROOT style for the graph, generally 'ATLAS'
-    :return: ROOT plots of the ratio of luminosities over the luminosity
-    '''
-    # Set ROOT graph style
-    set_style(str(style))
-
-
-    # Get ratio of the detectors
-    luminosity_ratio = []
-    for block in range(len(detector_one_data)):
-        for bcid in range(len(detector_one_data[block])):
-            ratio = -math.log(1 - detector_one_data[block][bcid]) / -math.log(1 - detector_two_data[block][bcid])
-            luminosity_ratio.append(ratio)
-
-    print(luminosity_ratio)
-    print(luminosity_blocks)
-    # create graph
-    graph = Graph(len(luminosity_blocks))
-    for i, (xx, yy) in enumerate(zip(luminosity_blocks, luminosity_ratio)):
-        graph.SetPoint(i, float(xx), float(yy))
-
-        # set visual attributes
-
-    graph.linecolor = 'white'  # Hides the lines at this time
-    graph.markercolor = 'blue'
-    graph.xaxis.SetTitle("Luminosity Block")
-    graph.yaxis.SetTitle("Luminosity")
-    graph.xaxis.SetRangeUser(min(luminosity_blocks), max(luminosity_blocks))
-    graph.yaxis.SetRangeUser(min(luminosity_ratio), max(luminosity_ratio))
-
-    # plot with ROOT
-    canvas = Canvas()
-    graph.Draw("APL")
-    wait(True)
-
-plot_luminosity_ratio(lucid_event_or_bi1, bcm_v_event_or1, luminosity_block, 'ATLAS')
+luminosity_plotting.plot_luminosity_ratio(bcm_h_event_or1, bcm_v_event_or1, luminosity_block, 'ATLAS')
---
+++ luminosity_plotting_routines.py
@@ -0,0 +1,52 @@
+from __future__ import division
+
+__author__ = 'jacob'
+import ROOT
+from rootpy.plotting import Canvas, Graph
+from rootpy.plotting.style import get_style, set_style
+from rootpy.interactive import wait
+import math
+
+
+def plot_luminosity_ratio(detector_one_data, detector_two_data, luminosity_blocks, style):
+    '''
+
+    :param detector_one_data: Data from one detector type, such as ATLAS' LUCID, in a list of lists, every entry is one
+    luminsoity block, with the luminosity block being a list of BCID data, assumed to be same
+    length as detector_two_data
+    :param detector_two_data: Data from another detector type, such as ATLAS' LUCID, in a list of lists, every entry is
+    one luminosity block, with the luminosity block being a list of BCID data, assumed to be same
+    length as detector_one_data
+    :param luminosity_blocks: A list of luminosity blocks
+    :param style: The ROOT style for the graph, generally 'ATLAS'
+    :return: ROOT plots of the ratio of luminosities over the luminosity
+    '''
+    # Set ROOT graph style
+    set_style(str(style))
+
+
+    # Get ratio of the detectors
+    luminosity_ratio = []
+    for block in range(len(detector_one_data)):
+        for bcid in range(len(detector_one_data[block])):
+            ratio = -math.log(1 - detector_one_data[block][bcid]) / -math.log(1 - detector_two_data[block][bcid])
+            luminosity_ratio.append(ratio)
+
+    # create graph
+    graph = Graph(len(luminosity_blocks))
+    for i, (xx, yy) in enumerate(zip(luminosity_blocks, luminosity_ratio)):
+        graph.SetPoint(i, float(xx), float(yy))
+
+    # set visual attributes
+
+    graph.linecolor = 'white'  # Hides the lines at this time
+    graph.markercolor = 'blue'
+    graph.xaxis.SetTitle("Luminosity Block")
+    graph.yaxis.SetTitle("Luminosity")
+    graph.xaxis.SetRangeUser(min(luminosity_blocks), max(luminosity_blocks))
+    graph.yaxis.SetRangeUser(min(luminosity_ratio), max(luminosity_ratio))
+
+    # plot with ROOT
+    canvas = Canvas()
+    graph.Draw("APL")
+    wait(True)
