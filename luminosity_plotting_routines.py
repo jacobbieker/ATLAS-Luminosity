63d46a9736c49f5960f90e57eae29961949e1801
Jacob Bieker <jacob@bieker.tech>
2016-03-03 12:40:11 -0800

    Remove points that have huge ratios
    Justification: These occur at the end of a run and correspond to conditions unlike the rest of the run

--- luminosity_plotting_routines.py
+++ luminosity_plotting_routines.py
@@ -520,8 +520,8 @@ def plot_all_luminosity_block_ratio(all_detector_one_data, all_detector_two_data
                 temp_detector_one.get(run)[block_count - 1].append(detector_one_avg)
                 temp_detector_two.get(run)[block_count - 1].append(detector_two_avg)
         # Remove the last luminosity block from each run, the one that generally spikes
-        temp_detector_one[run] = temp_detector_one[run][:-2]
-        temp_detector_two[run] = temp_detector_two[run][:-2]
+        temp_detector_one[run] = temp_detector_one[run][:-10]
+        temp_detector_two[run] = temp_detector_two[run][:-10]
     # Reassign temp to the original lists
     all_detector_one_data = temp_detector_one
     all_detector_two_data = temp_detector_two
@@ -560,7 +560,7 @@ def plot_all_luminosity_block_ratio(all_detector_one_data, all_detector_two_data
     graph.xaxis.SetTitle("Luminosity Block")
     graph.yaxis.SetTitle("Luminosity [Average Percent Ratio]")
     graph.xaxis.SetRangeUser(min(lumi_blocks), max(lumi_blocks))
-    graph.yaxis.SetRangeUser(-4.1, 6)
+    graph.yaxis.SetRangeUser(min(luminosity_ratio), max(luminosity_ratio))
 
     # plot with ROOT
     canvas = Canvas()
